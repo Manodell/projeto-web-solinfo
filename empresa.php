88fc4013148e35091ff17488bbf53f7c49fb659a
Manodell <manodell@gmail.com>
2015-02-26 09:50:34 -0300

    conclusao para entrega pro Eliezer

--- empresa.php
+++ empresa.php
@@ -24,8 +24,32 @@
         <li><a href="#" title="banner" alt="banner"><img src="images/product007.jpg"></a></li>
     </ul>
 </div>
+    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-4">
+        <div class="page-empresa page">
+            <h1 class="page-title"> Off Lord Jeans </h1>
+
+            <p>
+
+                A CADA DIA É UMA VITÓRIA, A <span>OFF LORD JEANS</span> CRECE, E PROPORCIONA CRESCIMENTO AOS NOSSOS REVENDEDORES, TUDO ISSO PELO QUADRO DE PROFISSIONAIS QUE A REGE. DESENVOLVENDO PRODUTOS DE VESTUÁRIO QUE TEM PROPORCIONADO AO CONSUMIDOR FINAL, QUALIDADE, CONFORTO E DURABILIDADE.
+
+            </p>
+
+            <p>DESDE 2009, A <span>OFF LORD JEANS</span>, TEM ATUADO NO MERCADO DA MODA, E SEMPRE ATUALIZANDO SUAS MODELAGENS COM ESTILO E MODERNIDADE. LOUVAMOS A DEUS, POR SUPERARMOS AS BARREIRAS E DIFICULDADES, QUE EM TODO ESSE TEMPO TEM COLABORADO PARA QUE a CADA DIA APRIMORARMOS COM MAIS AFINCO AO NOSSO TRABALHO</p>
+
+            <p>
+                SOMOS UMA EMPRESA COM O COMPROMISSO DE ATENDER JOVENS E ADULTOS COM LOOKS, E DESIGNER DA MODA ATUAL EM TODO O TEMPO.
+            </p>
+
+            <p>
+                POR ESSA RAZÃO agradecemos a VOCÊ, NOSSO CLIENTE.
+            </p>
+
+        </div>
+    </div>
 
 </div>
 </div>
+
 </section>
+
 <?php include("footer.php");?>
